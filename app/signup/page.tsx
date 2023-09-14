dc8485c95a3703a5a02930536ba0826b0356f614
Sakib all hasan <sakibul15.3883@gmail.com>
2023-09-14 23:24:29 +0600

    file and folder config and database connected

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,9 @@
+"use client"
+
 export default function LoginPage(){
     return (
         <div>
-        <h1>what,s up every one </h1>
+        <h1>what,s up every one, it,s a signup page  </h1>
 
         </div>
     )
