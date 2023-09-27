764a87ce18fcda68b18564f6336f6d4b24c6a42f
Sakib all hasan <sakibul15.3883@gmail.com>
2023-09-27 10:45:06 +0600

    database config done

--- dbConfig/dbConfig.ts
+++ dbConfig/dbConfig.ts
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 
 export async function connect() {
   try {
+
+    // the code mongoose.connect(process.env.MONGO_URL!) connects to a MongoDB database using the connection URL specified in the MONGO_URL environment variable. 
+
     mongoose.connect(process.env.MONGO_URL!);
+
+    // By assigning mongoose.connection to the connection variable, you are creating a reference to the Mongoose connection object. This reference allows you to interact with and listen for events related to the database connection.
+    
     const connection = mongoose.connection;
 
     connection.on("connected", () => {
