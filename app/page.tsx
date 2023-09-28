b0e624b7d4e96c6a98e8a961d4f9b5435fc1ad87
Sakib all hasan <sakibul15.3883@gmail.com>
2023-09-28 11:55:55 +0600

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-
+"use client";
+import { toast } from "react-hot-toast";
 import SignupPage from "@/app/signup/page"
 export default function Home() {
   return (
    <div>
-    <SignupPage/>
+  
    </div>
   )
 }
